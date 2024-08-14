3f3ae81b87b5c755c138a9d7c88184a8b178ed86
Aramjb <abubakarrabiuabubakar6@gmail.com>
2024-08-14 13:18:48 -0800

    initial commit

--- registration-form/src/app/app.component.ts
+++ registration-form/src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'registration-form';
+
+  registrationForm: FormGroup;
+
+  ngOnInit(){
+    this.registrationForm = new FormGroup ({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      gender: new FormControl(''),
+      address: new FormGroup({
+        street: new FormControl('', Validators.required),
+        country:new  FormControl('', Validators.required),
+        city: new  FormControl(''),
+        postal: new  FormControl('',Validators.required),
+        region: new  FormControl(''),
+      }),
+
+      skills: new FormArray([
+        new FormControl(null, Validators.required),
+        new FormControl(null, Validators.required),
+        new FormControl(null, Validators.required),
+      ])
+    })
+
+   
+  }
+
+  OnFormSubmitted(){
+    console.log(this.registrationForm.value);
+    this.registrationForm.reset()
+  }
 }
